b8a80611231bd34df5264ce0958ade8a2b27fa77
gvituri <gustavovituri@gmail.com>
2021-07-15 13:23:33 -0300

    Refactor changesign and erase btns

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ function powerCalc() {
     screenText.textContent = "_";         
     let lastOperand = "0";
     let currentOperand = "0";
+    let isPositive = true;
     clearMemory();
 
     /*Getting the functionality buttons and assigning functions*/
@@ -32,6 +33,15 @@ function powerCalc() {
 
     const btnBack = document.querySelector("#backspace");
     btnBack.onclick = eraseLast;
+
+    const btnChangeSign = document.querySelector("#negative");
+    btnChangeSign.addEventListener("click", e => {
+        if (currentOperand == "0") {
+            return;
+        }
+        currentOperand = changeSign(isPositive, currentOperand).toString();
+        screenText.textContent = `${currentOperand}`;
+    });
 
     getOperand();
 
@@ -49,9 +59,18 @@ function powerCalc() {
     
         for(let i = 0; i < btnNumbers.length; i++) {
             btnNumbers[i].addEventListener("click", e => {
-                if(btnNumbers[i].value == "." && currentOperand.includes(".")) {
+                if(currentOperand == "0") {
+                    if(btnNumbers[i].value == "0") {
+                        return;
+                    } else if(btnNumbers[i].value != ".") {
+                        currentOperand = btnNumbers[i].value;
+                        screenText.textContent = `${currentOperand}`;
+                        return;
+                    }
+                } else if (btnNumbers[i].value == "." && currentOperand.includes(".")) {
                     return;
                 }
+
                 currentOperand += btnNumbers[i].value;
                 screenText.textContent = `${currentOperand}`;
             });
@@ -65,15 +84,34 @@ function powerCalc() {
     }
 
     function eraseLast() {
-        if(currentOperand == "0") {
-            return;
-        }
-        
+        console.log(typeof(currentOperand));
+
         let operandArray = Array.from(currentOperand);
+        console.table(operandArray);
         operandArray.pop();
         console.table(operandArray);
+
+        if(operandArray.length == 1 && operandArray[0] == "-") {
+            operandArray = [];
+            operandArray.push("0");
+            isPositive = true;
+        } else if (operandArray[0] == "0") {
+            return;
+        } else if(operandArray.length == 0) {
+            operandArray.push("0");
+        }
+
         currentOperand = operandArray.join("");
-        console.log(currentOperand);
         screenText.textContent = currentOperand;  
+    }
+
+    function changeSign(curentSign, currentOperand) {
+
+        if(curentSign) {
+            isPositive = !isPositive;
+            return -Math.abs(currentOperand);
+        }
+        isPositive = !isPositive;
+        return Math.abs(currentOperand);
     }
 }
